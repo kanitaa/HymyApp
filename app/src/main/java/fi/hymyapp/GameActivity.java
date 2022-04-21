438d66f99868e9974fa22ea9da8c6893a567dbba
kanitaa <janita.kor@gmail.com>
2022-04-21 15:18:19 +0300

    kommentteja lisätty ja pieni bugi korjattu

--- app/src/main/java/fi/hymyapp/GameActivity.java
+++ app/src/main/java/fi/hymyapp/GameActivity.java
@@ -18,29 +18,34 @@ import com.google.firebase.database.ValueEventListener;
 
 public class GameActivity extends AppCompatActivity {
 
-
-
-    String questionText;
-    TextView questionView;
-
-    String dbpath;
-    int pathNumber=1;
-
-    private static final String TAG ="asd" ;
-    public int op1Value;
-    public int op2Value;
-    public int op3Value;
-    public int totalValue;
-
-
-
+    //init database and references to parts you want to access
     FirebaseDatabase database = FirebaseDatabase.getInstance("https://hymyapp-default-rtdb.europe-west1.firebasedatabase.app/");
     DatabaseReference op1Counter;
     DatabaseReference op2Counter;
     DatabaseReference op3Counter;
     DatabaseReference totalCounter;
-
     DatabaseReference statement;
+
+
+
+
+    //init values for path
+    String dbpath;
+    int pathNumber=1;
+
+    //init values for data you want to access, make their value = database value later
+    public int op1Value;
+    public int op2Value;
+    public int op3Value;
+    public int totalValue;
+    String statementText;
+
+    //set statement text here for ui
+    TextView statementView;
+
+    private static final String TAG ="Firebase" ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,14 +53,15 @@ public class GameActivity extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         int i =b.getInt(MainActivity.EXTRA,0);
 
+        //give db references path to database
         dbpath =Singleton.getInstance().getThemes().get(i).getDatapath();
         op1Counter = database.getReference(dbpath+"/question1/zOp1Count");
         op2Counter = database.getReference(dbpath+"/question1/zOp2Count");
         op3Counter = database.getReference(dbpath+"/question1/zOp3Count");
         totalCounter = database.getReference(dbpath+"/question1/zTotalCount");
-        statement = database.getReference(dbpath+"question1/aStatement");
+        statement = database.getReference(dbpath+"/question1/aStatement");
 
-        questionView = (TextView) findViewById(R.id.questionText);
+        statementView = (TextView) findViewById(R.id.statementText);
         ReadDataFromFirebase();
 
 
@@ -71,12 +77,14 @@ public class GameActivity extends AppCompatActivity {
     public void Option1(View view){
         final MediaPlayer mp = MediaPlayer.create(this,R.raw.sample);
         mp.start();
+        //when option is clicked, increase value by 1
         int newValue = op1Value+1;
+        //and set database value to match this new value
         op1Counter.setValue(newValue);
         newValue = totalValue+1;
         totalCounter.setValue(newValue);
 
-
+        //after increasing database value, show new question
         ChangeQuestion();
     }
     public void Option2(View view){
@@ -102,38 +110,43 @@ public class GameActivity extends AppCompatActivity {
     public void ChangeQuestion(){
         if(pathNumber!=2)
         {
+            //increase pathnumber by one, to access next question in database
             pathNumber+=1;
+            //change db references to match correct question number
+            dbpath = dbpath+"/question"+pathNumber;
+            statement =database.getReference(dbpath+"/aStatement");
+            op1Counter = database.getReference(dbpath+"/zOp1Count");
+            op2Counter = database.getReference(dbpath+"/zOp2Count");
+            op3Counter = database.getReference(dbpath+"/zOp3Count");
+            totalCounter = database.getReference(dbpath+"/zTotalCount");
+            ReadDataFromFirebase();
+
         }else{
-            Log.d(TAG, "Se oli vika kysymys paskaa");
+            //last question answered, change activity
             Intent lastActivity = new Intent(GameActivity.this,ResultsActivity.class);
             startActivity(lastActivity);
 
-            return;
         }
 
 
 
-        dbpath = dbpath+"/question"+pathNumber;
-        statement =database.getReference(dbpath+"/aStatement");
-        op1Counter = database.getReference(dbpath+"/zOp1Count");
-        op2Counter = database.getReference(dbpath+"/zOp2Count");
-        op3Counter = database.getReference(dbpath+"/zOp3Count");
-        totalCounter = database.getReference(dbpath+"/zTotalCount");
 
 
-        ReadDataFromFirebase();
+
     }
     public void ReadDataFromFirebase(){
 
-        //QUESTION TEXT
+        //QUESTION TEXT, add listener which triggers when data is changed
         statement.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                questionText = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Question is: " + questionText);
-                questionView.setText(questionText);
+                //after first time when data is changed, variable gets new value from database
+                statementText = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Question is: " + statementText);
+                //set new text to UI
+                statementView.setText(statementText);
 
             }
 
@@ -149,8 +162,7 @@ public class GameActivity extends AppCompatActivity {
         op1Counter.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+
                 op1Value = dataSnapshot.getValue(Integer.class);
                 Log.d(TAG, "Option1 Value is: " + op1Value);
 
@@ -168,8 +180,7 @@ public class GameActivity extends AppCompatActivity {
         op2Counter.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+
                 op2Value = dataSnapshot.getValue(Integer.class);
                 Log.d(TAG, "Option2 Value is: " + op2Value);
 
@@ -186,8 +197,7 @@ public class GameActivity extends AppCompatActivity {
         op3Counter.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+
                 op3Value = dataSnapshot.getValue(Integer.class);
                 Log.d(TAG, "Option3 Value is: " + op3Value);
 
@@ -205,8 +215,7 @@ public class GameActivity extends AppCompatActivity {
         totalCounter.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+
                 totalValue = dataSnapshot.getValue(Integer.class);
                 Log.d(TAG, "Value is: " + totalValue);
 
