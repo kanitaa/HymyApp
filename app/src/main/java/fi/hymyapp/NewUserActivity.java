bc4451dcb3092b1c4a7137ea636e171512affc65
Taru Vikström <taru.vikstrom@yahoo.com>
2022-04-27 13:14:28 +0300

    Input validaatio tsekki, hyvin pirkka, mutta pitäs toimii *DAP*

--- app/src/main/java/fi/hymyapp/NewUserActivity.java
+++ app/src/main/java/fi/hymyapp/NewUserActivity.java
@@ -1,16 +1,18 @@
 package fi.hymyapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class NewUserActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +23,17 @@ public class NewUserActivity extends AppCompatActivity {
     public void backToMainActivity(View view) {
 
         EditText editName = findViewById(R.id.editName);
-        String userName = editName.getText().toString();
         EditText editAge = findViewById(R.id.editAge);
+        String userName = editName.getText().toString();
         String userAge = editAge.getText().toString();
 
+        if(!ageInputValidator(userAge) | !nameInputValidator(userName)) {
+            return;
+        }
         Intent backToMainActivity = new Intent(NewUserActivity.this, MainActivity.class);
         userPrefEdit(userName, Integer.parseInt(userAge));
         startActivity(backToMainActivity);
+
     }
 
     public void userPrefEdit(String name, int age) {
@@ -39,8 +45,44 @@ public class NewUserActivity extends AppCompatActivity {
         userPrefEditor.commit();
     }
 
-    public void inputValidator(String name, String age) {
-        //joku validaatio systeemi?
+    public boolean nameInputValidator(String name) {
+
+        EditText editName = findViewById(R.id.editName);
+        Log.d(TAG, "nameInputValidator: tsekataan");
+
+        if(name.isEmpty()) {
+            editName.setError("Kenttää ei voi jättää tyhjäksi!");
+            return false;
+        } else if (name.length() > 12 || name.length() < 3) {
+            editName.setError("Nimen pitää olla 3-12 kirjainta.");
+            return false;
+        } else if (!name.matches("[a-zA-Z. -]*")) {
+            editName.setError("Käytä vain kirjaimia, välilyöntiä tai merkkejä . ja -");
+            return false;
+        } else {
+            editName.setError(null);
+            return true;
+        }
+    }
+
+    public boolean ageInputValidator(String age) {
+
+        EditText editAge = findViewById(R.id.editAge);
+        Log.d(TAG, "ageInputValidator: tsekataan");
+
+        if(age.isEmpty()) {
+            editAge.setError("Kenttää ei voi jättää tyhjäksi!");
+            return false;
+        } else if (!age.matches("[0-9]+")) {
+            editAge.setError("Iän täytyy olla numero.");
+            return false;
+        } else if ((Integer.parseInt(age)) < 1 || (Integer.parseInt(age)) > 99) {
+            editAge.setError("Iän täytyy olla väliltä 1-99.");
+            return false;
+        } else {
+            editAge.setError(null);
+            return true;
+        }
     }
 
 }
