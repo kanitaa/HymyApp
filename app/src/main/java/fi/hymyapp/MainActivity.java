902b9092953ca895da42416b76e6acaaed79fb33
Taru Vikström <taru.vikstrom@yahoo.com>
2022-05-11 16:03:06 +0300

    poistettu kirjatumis jutut ja activity sekä user class

--- app/src/main/java/fi/hymyapp/MainActivity.java
+++ app/src/main/java/fi/hymyapp/MainActivity.java
@@ -30,9 +30,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA = "com.example.myfirstapp.MESSAGE";
-    User user = new User();
-
-
     /**
      * Hide the support action bar
      * Add custon listView adapter to show themes from singleton class.
@@ -44,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        ListView lv =findViewById(R.id.themesListView);
+        ListView lv = findViewById(R.id.themesListView);
+        lv.setVisibility(GONE);
         //Custom layout for list items.
         lv.setAdapter(new ArrayAdapter<Themes>(this,
                 R.layout.adapter_view_layout,R.id.listItem,
@@ -62,14 +60,15 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent nextActivity=new Intent(MainActivity.this,GameActivity.class);
+                Intent nextActivity = new Intent(MainActivity.this,GameActivity.class);
                 nextActivity.putExtra(EXTRA,i);
                 startActivity(nextActivity);
             }
         });
+    }
 
-        userPrefReader(); // When starting the software the preferences need to be read to know if the user is new or known
-        updateUI(); // UI needs updating
+    public void start(View view) {
+        updateUI();
     }
 
     /**
@@ -80,59 +79,15 @@ public class MainActivity extends AppCompatActivity {
      * The text view is set to have a text that takes key values from preferences to greet the person by their name and age.
      */
     private void updateUI() { //This function can be called whenever changes have been made to the Activity elements and they need to be updated
-        Button continueButton = findViewById(R.id.newUserButton);
+        Button start = findViewById(R.id.startButton);
+        TextView greetText = findViewById(R.id.greetText);
         ListView lv = findViewById(R.id.themesListView);
-        TextView greetUser = findViewById(R.id.greetUserText);
-        ImageView logoImage = findViewById(R.id.logoImage);
-        SharedPreferences userPrefGet = getSharedPreferences("UserPrefs", Activity.MODE_PRIVATE);
+        ImageView logo = findViewById(R.id.logoImage);
 
-        if(userPrefGet.getBoolean("newUserKey", true)) { // If the user is new, the quizzes are not visible and continue button is visible
-            logoImage.setVisibility(View.VISIBLE);
-            continueButton.setVisibility(View.VISIBLE);
-            lv.setVisibility(GONE);
-            greetUser.setVisibility(GONE);
-        } else { // If the user is known, greeting text is visible, the quiz list is visible and the continue button is hidden
-            logoImage.setVisibility(GONE);
-            continueButton.setVisibility((GONE));
-            lv.setVisibility(View.VISIBLE);
-            greetUser.setText("Moi " + userPrefGet.getString("nameKey", "") + " " + userPrefGet.getInt("ageKey", 0) + "-vuotta ja tervetuloa kertomaan mielipiteesi lasten asioista!");
-            greetUser.setVisibility(View.VISIBLE);
-        }
-    }
-
-    /**
-     *  userPrefReader functions takes Shared preferences as a variable and reads the saved key values.
-     *  new user is created with the values gotten from the shared preferences. If there were no saved preferences, starter key values are given.
-     */
-    private void userPrefReader() { // This functions reads from the preferences
-        SharedPreferences userPrefGet = getSharedPreferences("UserPrefs", Activity.MODE_PRIVATE);
-        user = new User(userPrefGet.getString("nameKey", ""), userPrefGet.getInt("ageKey", 0), userPrefGet.getBoolean("newUserKey", true));
-    }
-
-    /**
-     * resetPrefs function resets the existing preferences back to the starter values and is bound to a button "uusi käyttäjä".
-     * Shared preferences and it's editor is taken as variables to change the values and commit them.
-     * updateUI function is called at the end to do the possible visual changes to the activity
-     * @param view a visual building block that responds to user inputs
-     */
-    public void resetPrefs(View view) { // This function resets the user prefs, then new user input is needed before quizzes open again
-        SharedPreferences userPrefPut = getSharedPreferences("UserPrefs", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor userPrefEditor = userPrefPut.edit();
-        userPrefEditor.putString("nameKey", ""); // Putting "empty" values for their corresponding keys
-        userPrefEditor.putInt("ageKey", 0);
-        userPrefEditor.putBoolean("newUserKey", true);
-        userPrefEditor.commit(); // Committing the changes to the preferences
-        updateUI(); // UI needs updating
-    }
-
-    /**
-     * openNewUserActivity function is bound to an on-click of a button "aloita" and is called when it's clicked.
-     * Intent is taken into a variable and it has the information of the starting activity and the activity that it starts, which is userInputActivity
-     * @param view a visual building block that responds to user inputs
-     */
-    public void openNewUserActivity(View view) { // This function is to onClick of the continue button that leads to a new activity
-        Intent userInputActivity = new Intent(MainActivity.this, NewUserActivity.class);
-        startActivity(userInputActivity); // Starting userInputActivity
+        start.setVisibility(GONE);
+        greetText.setVisibility(View.VISIBLE);
+        lv.setVisibility(View.VISIBLE);
+        logo.setVisibility(GONE);
     }
 
     /**
